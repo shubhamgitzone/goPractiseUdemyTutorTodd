a190abe64e7c4cca0ea60427ce6a3e99426fe33c
shubham kumar <shubhamkmuz@gmail.com>
2025-12-24 22:22:05 +0530

    creating exe for different os

--- 02helloWorldFromTerminal/main.go
+++ 02helloWorldFromTerminal/main.go
@@ -10,11 +10,15 @@ func main() {
 	// go run GoPractise/02helloWorldFromTerminal/main.go
 
 	// To build an executable file, use the command:
-	// go build -o helloWorldExecutable GoPractise/02helloWorldFromTerminal/main.go
+	// go build main.go
+	// To build on Linux / Mac, use the command:
+	// GOOS=linux go build -o helloWorldExecutable main.go
+	// GOOS=darwin go build -o helloWorldExecutable main.go
 
 	// After building, you can run the executable directly:
 	// On Windows:
 	// .\helloWorldExecutable.exe
 	// On Unix-based systems (Linux, macOS):
 	// ./helloWorldExecutable
+
 }
