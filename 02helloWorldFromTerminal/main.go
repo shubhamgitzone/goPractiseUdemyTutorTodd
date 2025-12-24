c20f752008c7449e69fd2b867b4fed5b0fbfac27
shubham kumar <shubhamkmuz@gmail.com>
2025-12-24 18:55:08 +0530

    02RunningAExecutableGo

--- 02helloWorldFromTerminal/main.go
+++ 02helloWorldFromTerminal/main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
 	fmt.Println("Hello! Gophers")
+
+	// To run this program, use the command:
+	// go run GoPractise/02helloWorldFromTerminal/main.go
+
+	// To build an executable file, use the command:
+	// go build -o helloWorldExecutable GoPractise/02helloWorldFromTerminal/main.go
+
+	// After building, you can run the executable directly:
+	// On Windows:
+	// .\helloWorldExecutable.exe
+	// On Unix-based systems (Linux, macOS):
+	// ./helloWorldExecutable
 }
