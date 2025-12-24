c0ccc76442dda7517ec10f29a60e45ae8640f5ef
shubham kumar <shubhamkmuz@gmail.com>
2025-12-24 18:38:37 +0530

    01creatingVars updated println

--- 01CreatingVars/main.go
+++ 01CreatingVars/main.go
@@ -9,8 +9,8 @@ const pi float64 = 3.14
 func main() {
 
 	fmt.Println(message)
-	fmt.Println("Value of pi constant:", pi)
+	fmt.Printf("Value of pi constant: %v and type of pi: %T\n", pi, pi)
 
 	count := 42
-	fmt.Println("Count variable inside function:", count)
+	fmt.Printf("Count variable inside function: %v and type of count : %T\n", count, count)
 }
