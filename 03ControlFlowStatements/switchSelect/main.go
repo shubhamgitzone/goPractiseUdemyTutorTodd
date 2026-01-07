5df98ace76e1a4683dd8c7277c79cc20cb7cf564
shubham kumar <shubhamkmuz@gmail.com>
2026-01-07 18:45:05 +0530

    conditional op

--- 03ControlFlowStatements/switchSelect/main.go
+++ 03ControlFlowStatements/switchSelect/main.go
@@ -17,6 +17,37 @@ func main() {
 	default:
 		fmt.Println("x is greater than or equal to 200")
 	}
+
+	y := rand.Intn(10)
+	z := rand.Intn(10)
+	fmt.Println("y =", y, "z =", z)
+
+	if y+z < 4 {
+		fmt.Println("y + z is less than 4")
+	} else if y+z >= 6 {
+		fmt.Println("y + z is greater than 6")
+	} else if y >= 4 && z <= 6 {
+		fmt.Println("y is greater than or equal to 4 and z is less than or equal to 6")
+	} else if y != 5 {
+		fmt.Println("y is not equal to 5")
+	} else {
+		fmt.Println("None of the above conditions met")
+	}
+
+	switch y + z {
+	case 0, 1, 2, 3:
+		fmt.Println("y + z is less than 4")
+	case 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18:
+		fmt.Println("y + z is greater than 6")
+	case 4, 5:
+		if y >= 4 && z <= 6 {
+			fmt.Println("y is greater than or equal to 4 and z is less than or equal to 6")
+		} else if y != 5 {
+			fmt.Println("y is not equal to 5")
+		}
+	default:
+		fmt.Println("None of the above conditions met")
+	}
 }
 
 func init() {
